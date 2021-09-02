cadefcdb3a3a5c9bdc5ed05cea797eb6eb4059ce
Syed Rishad <rishthefish21@gmail.com>
2021-09-01 21:20:36 -0400

    Delete simple-bar directory

--- simple-bar/lib/styles/components/data/wifi.js
+++
@@ -1,9 +0,0 @@
-export const wifiStyles = /* css */ `
-.wifi {
-  background-color: var(--red);
-}
-.simple-bar--background-color-as-foreground .wifi {
-  color: var(--red);
-  background-color: transparent;
-}
-`
--- simple-bar/lib/styles/pywal/pywal-gen.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-{
-  echo -n 'export const pywal = '
-  cat "$HOME/.cache/wal/colors-speedcrunch.json"
-  echo ';'
-} > pywal.js
